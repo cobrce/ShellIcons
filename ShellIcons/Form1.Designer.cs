002a7439380c33166eb7ced9bd1b8ff802d0a9ef
cobrce <cob_rce@hotmail.com>
2016-04-10 14:13:17 +0100

    https://github.com/cobrce/ShellIcons.git

---
+++ ShellIcons/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ShellIcons
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader0 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnSetIcons = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader0,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1119, 389);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "key";
+            this.columnHeader1.Width = 220;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Command";
+            this.columnHeader2.Width = 310;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Icon";
+            this.columnHeader3.Width = 378;
+            // 
+            // columnHeader0
+            // 
+            this.columnHeader0.Text = "type";
+            this.columnHeader0.Width = 82;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnSetIcons);
+            this.splitContainer1.Panel2.Controls.Add(this.btnRefresh);
+            this.splitContainer1.Size = new System.Drawing.Size(1119, 439);
+            this.splitContainer1.SplitterDistance = 389;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(12, 11);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 0;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnSetIcons
+            // 
+            this.btnSetIcons.Location = new System.Drawing.Point(93, 11);
+            this.btnSetIcons.Name = "btnSetIcons";
+            this.btnSetIcons.Size = new System.Drawing.Size(75, 23);
+            this.btnSetIcons.TabIndex = 1;
+            this.btnSetIcons.Text = "Set icons";
+            this.btnSetIcons.UseVisualStyleBackColor = true;
+            this.btnSetIcons.Click += new System.EventHandler(this.btnSetIcons_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 439);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SellIcon by COB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader0;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnSetIcons;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ ShellIcons/Form1.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShellIcons
+{
+    public partial class Form1 : Form
+    {
+        RegistryKey dllFile, exeFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshKeys();
+        }
+
+        private void RefreshKeys()
+        {
+            listView1.Items.Clear();
+
+            dllFile = Registry.ClassesRoot.OpenSubKey("dllfile\\shell", false);
+            exeFile = Registry.ClassesRoot.OpenSubKey("exefile\\shell", false);
+
+            listView1.Items.AddRange(CreateListViewItems("exe", exeFile));
+            listView1.Items.AddRange(CreateListViewItems("dll", dllFile));
+
+            foreach (ColumnHeader column in listView1.Columns)
+                column.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+        private ListViewItem[] CreateListViewItems(string type, RegistryKey key)
+        {
+            string[] subkeys = key.GetSubKeyNames();
+
+            List<ListViewItem> lvis = new List<ListViewItem>();
+            foreach (string subkey in subkeys)
+            {
+                if (subkey.ToLower() == "open" || subkey.ToLower() == "runas") continue;
+                string val = "";
+                string icon = "";
+                using (RegistryKey item = key.OpenSubKey(subkey))
+                {
+                    icon = (string)item.GetValue("Icon", "");
+                    using (RegistryKey command = item.OpenSubKey("Command"))
+                    {
+                        val = (string)command.GetValue("");
+                    }
+                }
+                if (val == null || val == "") continue;
+
+                bool check = false;
+                if (icon == null || icon == "")
+                {
+                    check = true;
+                    icon = "\"" + val.Split(val.StartsWith("\"") ? '"' : '%')[val.StartsWith("\"") ? 1 : 0] + "\",0";
+                }
+
+
+                ListViewItem lvi = new ListViewItem(new string[] { type, subkey, val, icon });
+                if (check)
+                {
+                    lvi.Checked = true;
+                    lvi.BackColor = Color.SkyBlue;
+                }
+                lvis.Add(lvi);
+            }
+            return lvis.ToArray();
+        }
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshKeys();
+        }
+
+        private void btnSetIcons_Click(object sender, EventArgs e)
+        {
+            List<string> errors = new List<string>();
+            foreach(ListViewItem lvi in listView1.Items)
+            {
+                if (!lvi.Checked) continue;
+                try
+                {                    
+                    RegistryKey key = (lvi.SubItems[0].Text == "dll") ? dllFile : exeFile;
+                    RegistryKey item = key.OpenSubKey(lvi.SubItems[1].Text, true);
+                    item.SetValue("Icon", lvi.SubItems[3].Text);
+                    item.Close();
+                }
+                catch { errors.Add(lvi.Index.ToString()); }
+            }
+            string message = (errors.Count == 0) ? "Done!" : "Errors found in the following index" + Environment.NewLine + string.Join(",", errors);
+            MessageBox.Show(message);
+        }
+    }
+}
